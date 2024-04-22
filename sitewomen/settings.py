377a0a24dbeee88dc78ccc39b121f9d091d051fb
Sergei Bykov <serega1988bykov@gmail.com>
2024-04-22 16:45:50 +0500

    add photo to adminboard

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -112,6 +112,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 
--- women/views.py
+++ women/views.py
@@ -62,12 +62,7 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
-            """# print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, "Ошибка добавления поста")"""
+            # print(form.cleaned_data)
             form.save()
             redirect('home')
     else:
