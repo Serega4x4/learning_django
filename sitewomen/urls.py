5a212155fa8c4605d636f18483034159ef59c58f
Sergei Bykov <serega1988bykov@gmail.com>
2024-05-06 08:27:16 +0500

    Github Auth

--- sitewomen/urls.py
+++ sitewomen/urls.py
@@ -3,19 +3,20 @@ from django.contrib import admin
 from django.urls import path, include
 
 from sitewomen import settings
+from women import views
 from women.views import page_not_found
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls')),
     path('users/', include('users.urls', namespace="users")),
     path("__debug__/", include("debug_toolbar.urls")),
-    path('social-auth/', include('social_django.urls', namespace='social')), 
+    path('social-auth/', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 handler404 = page_not_found
 
