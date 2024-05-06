b6178adc537241b51868160d45a25da00f8458f8
Sergei Bykov <serega1988bykov@gmail.com>
2024-05-06 15:31:39 +0500

    for show

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -152,19 +152,23 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.yandex.ru"
 EMAIL_PORT = 465
-########################
+EMAIL_HOST_USER = "ggg@myandex.ru"
+EMAIL_HOST_PASSWORD = "gheruiaghsfdopgahuoe9"
 EMAIL_USE_SSL = True
 
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-# SERVER_EMAIL = EMAIL_HOST_USER
-# EMAIL_ADMIN = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
 
 AUTH_USER_MODEL = 'users.User'
 
 DEFAULT_USER_IMAGE = MEDIA_URL + 'users/default.png'
 
+EMAIL_HOST_USER = "jurtewjhtrwjhwr@myandex.ru"
+EMAIL_HOST_PASSWORD = "hzvftqhjwrtjhrxjkxhvnfod"
 
-##################################
+SOCIAL_AUTH_GITHUB_KEY = 'jtwjhtrwhrtwhtrwhrtht'
+SOCIAL_AUTH_GITHUB_SECRET = 'jrtweujwtgerah'
 
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
